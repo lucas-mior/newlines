6e56ba70fe99a2c07f7545243e6f8dc0314369a6
lucas-mior <lucas.mior@tutamail.com>
2025-10-10 07:05:39 -0300

    exit

--- new_lines.c
+++ new_lines.c
@@ -5,12 +5,12 @@ int main(int argc, char *argv[]) {
     int n;
 
     if (argc < 2)
-        return 0;
+        exit(EXIT_FAILURE);
 
     if ((n = atoi(argv[1])) <= 0)
-        return 0;
+        exit(EXIT_FAILURE);
 
     for (int i = 0; i < n; i += 1)
         (void) putchar('\n');
-    return 0;
+    exit(EXIT_SUCCESS);
 }
