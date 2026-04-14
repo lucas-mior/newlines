4e3f439bb4b90cba17ffe67b772994bfdf312d0d
lucas-mior <lucas.mior@tutamail.com>
2026-04-14 15:18:39 -0300

    nolibc.c

--- newlines_amd64.c
+++ newlines_amd64.c
@@ -1,31 +1,11 @@
-#include <sys/syscall.h>
 #include <unistd.h>
+#include "nolibc.c"
 
 #if defined(__GNUC__) || defined(__INTEL_COMPILER) || defined(__clang__)
 #define UNLIKELY(x) __builtin_expect(x, 0)
 #else
 #define UNLIKELY(x) (x)
 #endif
-
-__asm__(".global _start\n"
-        "_start:\n"
-        "   movl  (%rsp), %edi\n"   // argc -> %edi
-        "   lea   8(%rsp), %rsi\n"  // argv -> %rsi
-        "   call  main\n"           // call main(argc, argv)
-        "   movl  %eax, %edi\n"     // exit code -> %edi
-        "   movl  $60, %eax\n"      // SYS_exit
-        "   syscall\n"              // exit(%eax)
-);
-
-static inline long __attribute__((always_inline))
-syscall3(long n, long a1, void *a2, long a3) {
-    long ret;
-    asm volatile("syscall"
-                 : "=a"(ret)
-                 : "a"(n), "D"(a1), "S"(a2), "d"(a3)
-                 : "rcx", "r11", "memory");
-    return ret;
-}
 
 static inline int __attribute__((always_inline)) atoi(const char *s);
 
---
+++ nolibc.c
@@ -0,0 +1,23 @@
+#include <sys/syscall.h>
+#include <unistd.h>
+
+__asm__(".global _start\n"
+        "_start:\n"
+        "   movl  (%rsp), %edi\n"   // argc -> %edi
+        "   lea   8(%rsp), %rsi\n"  // argv -> %rsi
+        "   call  main\n"           // call main(argc, argv)
+        "   movl  %eax, %edi\n"     // exit code -> %edi
+        "   movl  $60, %eax\n"      // SYS_exit
+        "   syscall\n"              // exit(%eax)
+);
+
+static inline long __attribute__((always_inline))
+syscall3(long n, long a1, void *a2, long a3) {
+    long ret;
+    asm volatile("syscall"
+                 : "=a"(ret)
+                 : "a"(n), "D"(a1), "S"(a2), "d"(a3)
+                 : "rcx", "r11", "memory");
+    return ret;
+}
+
